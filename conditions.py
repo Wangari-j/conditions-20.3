558dd101e1a2188d87e0081c35e367c9ee7d1a26
Wangari-j <wangarijudy@gmail.com>
2024-03-20 23:09:42 +0300

    conditions

---
+++ conditions.py
@@ -0,0 +1,12 @@
+# If = Do some code if some condition is True
+# else do something else
+
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are now signed up!")
+elif age <= 0:
+    print("You are not yet born!")
+else:
+    print("You must be 18+ to sign up")
